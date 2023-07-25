1927ce639b3f00ec5d95be64c8854fb20915e265
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-25 10:28:26 -0300

    refactor: code styling

--- src/components/bgSelectProfession/index.tsx
+++ src/components/bgSelectProfession/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as S from "./styles";
 import { Container } from "../container";
-// import bg from "../../assets/bgdesktop.jpg";
+
 interface IChildren {
     children: React.ReactNode
 }
