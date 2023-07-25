9574531fdd60920c89174167bcac19e79ec2d303
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-25 10:40:16 -0300

    feat: added element logo for page

--- src/pages/selectProfession/index.tsx
+++ src/pages/selectProfession/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BgSelect } from "../../components/bgSelectProfession";
+import { Logo } from "../../components/logo";
 
 export const SelectProfession:React.FC = () => {
 	return (
 		<BgSelect>
-
+			<Logo/>
 		</BgSelect>
 	);
 };
