a96d78eb3be1aad909e09915b7bebf620316f0a4
Reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2023-07-24 23:41:02 -0300

    feat: configure react-router-dom and add SelectProfession in route '/'

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,19 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom";
+import { SelectProfession } from "./pages/selectProfession";
 
-function App() {
-	const [count, setCount] = useState(0);
+export const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <SelectProfession/>
+	}
+]);
 
+export const Router:React.FC = () => {
 	return (
-		<><div>
-			<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-				<img src={viteLogo} className="logo" alt="Vite logo" />
-			</a>
-			<a href="https://react.dev" target="_blank" rel="noreferrer">
-				<img src={reactLogo} className="logo react" alt="React logo" />
-			</a>
-		</div><h1>Vite + React</h1><div className="card">
-			<button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-			</button>
-			<p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-			</p>
-		</div><p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-		</p></>
+		<RouterProvider router={router} />
 	);
-}
-
-export default App;
+};
