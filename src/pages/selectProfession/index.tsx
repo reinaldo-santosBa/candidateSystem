fb6dd3b66f7c4399804c43b9271019fd6b5b2992
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-25 23:04:05 -0300

    feat: add component Button select in page

--- src/pages/selectProfession/index.tsx
+++ src/pages/selectProfession/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BgSelect } from "../../components/bgSelectProfession";
 import { Logo } from "../../components/logo";
+import { ButtonSelect } from "../../components/btnSelect";
 
 export const SelectProfession:React.FC = () => {
 	return (
 		<BgSelect>
-			<Logo/>
+			<Logo />
+			<ButtonSelect/>
 		</BgSelect>
 	);
 };
