2803275e1582c35d7eac3aa7d5b6def731d4c4fa
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-26 23:05:48 -0300

    feat: add buttonselect

--- src/pages/selectProfession/index.tsx
+++ src/pages/selectProfession/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BgSelect } from "../../components/bgSelectProfession";
 import { Logo } from "../../components/logo";
 import { ButtonSelect } from "../../components/btnSelect";
+import { BtnGoMainPage } from "../../components/btnGoMainPage";
 
 export const SelectProfession:React.FC = () => {
 	return (
 		<BgSelect>
 			<Logo />
-			<ButtonSelect/>
+			<ButtonSelect />
+			<BtnGoMainPage/>
 		</BgSelect>
 	);
 };
