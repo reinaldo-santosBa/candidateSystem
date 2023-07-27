451b3e1401a9a2d3170bc22af21af2d2fe491a77
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-26 23:16:11 -0300

    feat: add link

--- src/components/btnGoMainPage/index.tsx
+++ src/components/btnGoMainPage/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import * as S from "./styles";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/context";
 
 export const BtnGoMainPage: React.FC = () => {
-
+	const {
+		id,
+	} = useContext(AuthContext);
 	return (
 		<S.AreaBtnGoHome>
-			<Link className="textBtn" to="/home">About Us</Link>
+			<Link className="textBtn" to={"/home/" + id}>Entrar</Link>
 		</S.AreaBtnGoHome>
 	);
 };
