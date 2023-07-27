55721eb9db759d1bdbac5ad4b1fd5a43f100fb83
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-26 23:15:05 -0300

    feat: add contextapi

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {Router} from "./App.tsx";
 import "./styles.css";
+import { AuthContextProvider } from "./context/context.tsx";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<Router />
+		<AuthContextProvider>
+			<Router />
+		</AuthContextProvider>
+		
 	</React.StrictMode>,
 );
