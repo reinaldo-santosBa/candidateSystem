aa57f7506c0b3d9b9a20e7ee2fef2e478ffd1af0
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-29 12:53:53 -0300

    feat: called menu element

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Container } from "../../components/container";
+import { Menu } from "../../components/menu";
 
 export const Home: React.FC = () => {
 	const { id } = useParams();
 	return (
-		<>{id}</>
+		<Container>
+			<Menu>
+				{id}
+			</Menu>
+		</Container>
 	);
 };
