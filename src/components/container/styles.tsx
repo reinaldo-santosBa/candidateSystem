555772315f182abe977e2a94fd9b596303b19fa7
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-29 12:53:23 -0300

    refactor: add css for min and max to full size and change background color

--- src/components/container/styles.tsx
+++ src/components/container/styles.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
+    min-width: 100vw;
+    min-height: 100vh;
+    max-width: 100vw;
+    max-height: 100vh;
+    background-color: #1E1E1E;
 `;
