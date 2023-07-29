729e4cf890dd841d7603b2ae86be88444716b13e
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-29 11:37:18 -0300

    feat: create a new instance for arrayProfession

---
+++ src/constant/arrayProfession.ts
@@ -0,0 +1,22 @@
+export const arrayProfession = [
+	{
+		id: 1,
+		name: "Front - End"
+	},
+	{
+		id: 2,
+		name: "Back - End"
+	},
+	{
+		id: 3,
+		name: "Full Stack"
+	},
+	{
+		id: 4,
+		name: "Designer Figma"
+	},
+	{
+		id: 5,
+		name: "Banco de Talentos"
+	},
+];
