8acae01735e0f5f169df15a1963ed96440ecabfa
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-29 11:37:44 -0300

    feat: create a custom single select

---
+++ src/components/select/index.tsx
@@ -0,0 +1,82 @@
+import React, { useContext, useState } from "react";
+import * as S from "./styles";
+import { IoMdArrowDropup } from "react-icons/io";
+import { arrayProfession } from "../../constant/arrayProfession";
+import { AuthContext } from "../../context/context";
+
+interface IArray {
+	id: number;
+	name: string;
+}
+
+
+const Select: React.FC = () => {
+	const {
+		setId,
+	} = useContext(AuthContext);
+	const [active, setActive] = useState("disabled");
+	const [textCamp, setTextCamp] = useState("Escolha um processo seletivo");
+	const [rotate, setRotate] = useState("rotate(270deg)");
+	const handleClick = (text: string, id: number) => {
+		if (active === "disabled") {
+			setActive("active");
+			setRotate("rotate(180deg)");
+			
+		} else {
+			setActive("disabled");
+			setRotate("rotate(270deg)");
+		}
+		setTextCamp(text);
+		setId(id);
+	};
+
+	return (
+		<S.AreaSelect>
+			<S.Campo
+				rotate={rotate}
+				onClick={
+					() => {
+						handleClick("Escolha um processo seletivo",0);
+					}
+				}
+			>
+				<p>
+					{textCamp}
+				</p>	
+				<IoMdArrowDropup className={"icon"} />
+			</S.Campo>
+			<S.AreaItems
+			>
+				{arrayProfession.map(
+					(item: IArray) => {
+						return (
+							<div
+								className={`item ${active}`}
+								key={item.id}
+								onClick={
+									() => {
+										handleClick(item.name, item.id);
+									}
+								}
+							>
+								<p>{item.name}</p>
+							</div>
+						);
+					}
+				)}
+
+			</S.AreaItems>
+
+		</S.AreaSelect>
+	);
+};
+
+export default Select;
+
+/*
+1. Front - End
+2. Back - End
+3. Full Stack
+4. Designer Figma
+5. Banco de Talentos
+*/
---
+++ src/components/select/styles.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+interface ElementIconProps {
+    rotate: string;
+}
+export const AreaSelect = styled.div`
+    width: auto;
+    height: auto;
+    width: 20rem;
+`;
+
+
+export const Campo = styled.div <ElementIconProps>`
+    background-color: #096688;
+    color: #FFF;
+    padding: 1rem;
+    cursor: pointer;
+    display: flex;
+    font-size: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    gap: .7rem;
+    .icon{
+        transform: ${props => props.rotate};
+        transition: all .5s ease-in-out;
+        font-size: 2rem;
+    }
+`;
+
+export const AreaItems = styled.div`
+
+    width: 100%;
+    height: 0;
+    transition: all .1s ease-in-out;
+    display: inline-block;
+    .item{
+        background-color: #096688;
+        color: #FFF;
+        font-size: 1.2rem;
+        padding: 1rem;
+        cursor: pointer;
+        transition: all .5s ease-in-out;
+        z-index: 100;
+    }
+    .item.active{
+        visibility: visible;
+        opacity: 1;
+    }
+    .item.disabled{
+        visibility: hiden;
+        opacity: 0;
+    }
+`;
