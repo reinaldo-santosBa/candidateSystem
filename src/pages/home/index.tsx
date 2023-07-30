0d977ecff4da2151fa409b27081501ee78c53c00
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 13:21:39 -0300

    feat: add idMenu

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { Container } from "../../components/container";
 import { Menu } from "../../components/menu";
+import { Logo } from "../../components/logo";
 
 export const Home: React.FC = () => {
 	const { id } = useParams();
 	return (
 		<Container>
-			<Menu>
+			<Menu
+				idMenu={1}
+			>
+				<Logo/>
 				{id}
 			</Menu>
+			
 		</Container>
 	);
 };
