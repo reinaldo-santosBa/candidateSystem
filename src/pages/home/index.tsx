5aa3f010352e55bf8ce6b353ab436c5cc3ed9a5e
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 17:59:18 -0300

    feat:  add ContentHome

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { Container } from "../../components/container";
 import { Menu } from "../../components/menu";
 import { Logo } from "../../components/logo";
+import { ContentHome } from "../../components/ContentHome";
 
 export const Home: React.FC = () => {
 	const { id } = useParams();
@@ -12,9 +13,10 @@ export const Home: React.FC = () => {
 				idMenu={1}
 			>
 				<Logo/>
-				{id}
 			</Menu>
-			
+			<ContentHome
+				id={Number(id)}
+			/>
 		</Container>
 	);
 };
