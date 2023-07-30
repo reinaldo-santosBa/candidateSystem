7a6c5ce54d0daadeae002e5dcd5d996019eefaeb
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 17:56:56 -0300

    feat: create arrayCandidates

---
+++ src/constant/arrayCandidates.ts
@@ -0,0 +1,361 @@
+
+interface ArrayCandidates {
+	id: number,
+	name: string,
+	idProfession: number,
+	image: string,
+	selected: number
+}
+export const arrayCandidates: ArrayCandidates[] = [
+	{
+		id: 1,
+		name: "Ana Silva",
+		idProfession: 1,
+		image: "https://cdn.pixabay.com/photo/2017/08/25/21/45/thinking-2681494_640.jpg",
+		selected: 0
+	},
+	{
+		id: 2,
+		name: "Pedro Santos",
+		idProfession: 1,
+		image: "https://cdn.pixabay.com/photo/2015/07/20/12/57/ambassador-852766_640.jpg",
+		selected: 0
+	},
+	{
+		id: 3,
+		name: "Marina Costa",
+		idProfession: 1,
+		image: "https://images.unsplash.com/photo-1591782151563-36ca6da76873?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fGZyZW50ZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 4,
+		name: "Gabriel Oliveira",
+		idProfession: 1,
+		image: "https://cdn.pixabay.com/photo/2016/03/24/09/10/man-1276384_640.jpg",
+		selected: 0
+	},
+	{
+		id: 5,
+		name: "Carolina Rocha",
+		idProfession: 1,
+		image: "https://plus.unsplash.com/premium_photo-1671656333511-1b43049ea1a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cm9zdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+		
+	},
+	{
+		id: 6,
+		name: "Lucas Almeida",
+		idProfession: 1,
+		image: "https://cdn.pixabay.com/photo/2019/04/15/11/47/man-4129126_640.jpg",
+		selected: 0
+	},
+	{
+		id: 7,
+		name: "Isabela Fernandes",
+		idProfession: 1,
+		image: "https://images.unsplash.com/photo-1593529467220-9d721ceb9a78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 8,
+		name: "Rafael Lima",
+		idProfession: 1,
+		image: "https://cdn.pixabay.com/photo/2014/04/12/14/59/portrait-322470_640.jpg",
+		selected: 0
+	},
+	{
+		id: 9,
+		name: "Beatriz Castro",
+		idProfession: 1,
+		image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cm9zdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60 ",
+		selected: 0
+	},
+	{
+		id: 10,
+		name: "Matheus Rodrigues",
+		idProfession: 1,
+		image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9zdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 11,
+		name: "Mariana Ribeiro",
+		idProfession: 2,
+		image: "https://images.unsplash.com/photo-1557296387-5358ad7997bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cm9zdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 12,
+		name: "Thiago Barbosa",
+		idProfession: 2,
+		image: "https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cm9zdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 13,
+		name: "Camila Pereira",
+		idProfession: 2,
+		image: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cm9zdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 14,
+		name: "Gustavo Cardoso",
+		idProfession: 2,
+		image: "https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cm9zdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 15,
+		name: "Júlia Santos",
+		idProfession: 2,
+		image: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cm9zdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 16,
+		name: "Leonardo Costa",
+		idProfession: 2,
+		image: "https://plus.unsplash.com/premium_photo-1680020185326-45491267f8da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cm9zdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 17,
+		name: "Letícia Ferreira",
+		idProfession: 2,
+		image: "https://plus.unsplash.com/premium_photo-1664203068007-52240d0ca48f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 18,
+		name: "André Alves",
+		idProfession: 2,
+		image: "https://images.unsplash.com/photo-1545996124-0501ebae84d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 19,
+		name: "Fernanda Rocha",
+		idProfession: 2,
+		image: "https://plus.unsplash.com/premium_photo-1682965699742-92e41c2d8630?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 20,
+		name: "Bruno Oliveira",
+		idProfession: 2,
+		image: "https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 21,
+		name: "Luiza Torres",
+		idProfession: 3,
+		image: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 22,
+		name: "Felipe Castro",
+		idProfession: 3,
+		image: "https://images.unsplash.com/photo-1544348817-5f2cf14b88c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 23,
+		name: "Manuela Carvalho",
+		idProfession: 3,
+		image: "https://plus.unsplash.com/premium_photo-1682965455667-ddf5eb44b73e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 24,
+		name: "Matheus Gomes",
+		idProfession: 3,
+		image: "https://images.unsplash.com/photo-1560787313-5dff3307e257?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 25,
+		name: "Giovanna Lima",
+		idProfession: 3,
+		image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 26,
+		name: "Guilherme Sousa",
+		idProfession: 3,
+		image: "https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 27,
+		name: "Isabela Santos",
+		idProfession: 3,
+		image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 28,
+		name: "Rafaela Costa",
+		idProfession: 3,
+		image: "https://images.unsplash.com/photo-1578489758854-f134a358f08b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 29,
+		name: "Lucas Rocha",
+		idProfession: 3,
+		image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 30,
+		name: "Carolina Oliveira",
+		idProfession: 3,
+		image: "https://images.unsplash.com/photo-1618835962148-cf177563c6c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 31,
+		name: "Enzo Ferreira",
+		idProfession: 4,
+		image: "https://plus.unsplash.com/premium_photo-1664453547090-0e8a92a6ed4c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 32,
+		name: "Sophia Almeida",
+		idProfession: 4,
+		image: "https://images.unsplash.com/photo-1611594547712-9e5d7da58684?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 33,
+		name: "Miguel Pereira",
+		idProfession: 4,
+		image: "https://images.unsplash.com/photo-1570158268183-d296b2892211?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 34,
+		name: "Laura Rodrigues",
+		idProfession: 4,
+		image: "https://images.unsplash.com/photo-1573140247632-f8fd74997d5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 35,
+		name: "Pedro Martins",
+		idProfession: 4,
+		image: "https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 36,
+		name: "Valentina Ribeiro",
+		idProfession: 4,
+		image: "https://images.unsplash.com/photo-1602481182506-6e15d0d2e44b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 37,
+		name: "Arthur Lima",
+		idProfession: 4,
+		image: "https://images.unsplash.com/photo-1611695434398-4f4b330623e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 38,
+		name: "Gabriela Castro",
+		idProfession: 4,
+		image: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 39,
+		name: "Lucas Barbosa",
+		idProfession: 4,
+		image: "https://images.unsplash.com/photo-1556942057-94aaf3ae5d6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDh8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 40,
+		name: "Isabella Cardoso",
+		idProfession: 4,
+		image: "https://images.unsplash.com/photo-1595956553066-fe24a8c33395?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 41,
+		name: "Rafaela Costa",
+		idProfession: 5,
+		image: "https://images.unsplash.com/photo-1603775020644-eb8decd79994?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 42,
+		name: "Mateus Fernandes",
+		idProfession: 5,
+		image: "https://images.unsplash.com/photo-1595956553066-fe24a8c33395?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 43,
+		name: "Juliana Santos",
+		idProfession: 5,
+		image: "https://plus.unsplash.com/premium_photo-1671641797308-26be77d80a2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDl8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 44,
+		name: "Gabriel Oliveira",
+		idProfession: 5,
+		image: "https://plus.unsplash.com/premium_photo-1677191563928-24aa5cc54159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 45,
+		name: "Manuela Silva",
+		idProfession: 5,
+		image: "https://images.unsplash.com/photo-1611310424006-42cf1e064288?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTR8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 46,
+		name: "André Almeida",
+		idProfession: 5,
+		image: "https://images.unsplash.com/photo-1591973669966-52d2534d9087?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 47,
+		name: "Isabela Ribeiro",
+		idProfession: 5,
+		image: "https://images.unsplash.com/flagged/photo-1578025468422-e69c574eb76a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 48,
+		name: "Leonardo Pereira",
+		idProfession: 5,
+		image: "https://images.unsplash.com/photo-1587064712555-6e206484699b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjZ8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 49,
+		name: "Marina Rodrigues",
+		idProfession: 5,
+		image: "https://images.unsplash.com/photo-1608190003443-86b2636f2fe3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzZ8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+	{
+		id: 50,
+		name: "Lucas Souza",
+		idProfession: 5,
+		image: "https://images.unsplash.com/photo-1615813967515-e1838c1c5116?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzV8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+		selected: 0
+	},
+];
