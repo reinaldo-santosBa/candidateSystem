1a08d300f40663efd5ab4047a37013117e3f93d8
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 17:57:26 -0300

    feat: create candidateselects

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -3,12 +3,18 @@ import React, { SetStateAction, createContext, useState } from "react";
 interface IProvider {
     children: React.ReactNode
 }
+interface ICandidatesSelects{
+	idProfession: number;
+	idCandidate: number;
+}
 
 interface IProps {
 	idProfession: number;
 	setIdProfession: React.Dispatch<SetStateAction<number>>;
 	att: boolean;
 	setAtt: React.Dispatch<SetStateAction<boolean>>;
+	candidatesSelects: Array<ICandidatesSelects>
+	setCandidatesSelects: React.Dispatch<SetStateAction<ICandidatesSelects[]>>;
 }
 
 export const AuthContext = createContext<IProps>({} as IProps);
@@ -16,16 +22,41 @@ export const AuthContext = createContext<IProps>({} as IProps);
 export const AuthContextProvider: React.FC<IProvider> = ({ children }) => {
 	const [idProfession, setIdProfession] = useState(0);
 	const [att, setAtt] = useState(false);
+	const [candidatesSelects, setCandidatesSelects] = useState([
+		{
+			idProfession: 1,
+			idCandidate: 0
+		},
+		{
+			idProfession: 2,
+			idCandidate: 0
+		},
+		{
+			idProfession: 3,
+			idCandidate: 0
+		},
+		{
+			idProfession: 4,
+			idCandidate: 0
+		},
+		{
+			idProfession: 5,
+			idCandidate: 0
+		},
+
+	]);
 	return (
 		<AuthContext.Provider
 
 			value={
 
 				{
-					idProfession: idProfession,
-					setIdProfession: setIdProfession,
-					att: att,
-					setAtt: setAtt,
+					idProfession,
+					setIdProfession,
+					att,
+					setAtt,
+					candidatesSelects,
+					setCandidatesSelects
 				}
 
 			}
