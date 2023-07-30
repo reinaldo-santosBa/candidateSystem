dbff58d608c55c32076de3f5c6dda1dee780ddb6
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 13:19:49 -0300

    feat: add important css

--- src/components/logo/styles.tsx
+++ src/components/logo/styles.tsx
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const Logo = styled.img`
     height: 100%;
+    margin: 0 auto;
     width: 100%;
     max-width: 9.375rem;
     max-height: 9.375rem;
-    margin-top: 3.125rem;
+    margin-top: 3.125rem !important;
 `;
