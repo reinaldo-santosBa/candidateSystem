9fa443a3416aef24f29bdc8818441b93661a427c
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 17:57:50 -0300

    feat: add overflow-x: auto

--- src/components/container/styles.tsx
+++ src/components/container/styles.tsx
@@ -4,6 +4,7 @@ export const Container = styled.div`
     min-width: 100vw;
     min-height: 100vh;
     max-width: 100vw;
-    max-height: 100vh;
+
     background-color: #1E1E1E;
+    overflow-x: auto;
 `;
