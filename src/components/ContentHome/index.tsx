0987b4e5552bfdd7c58ec4d013159879ae7bde4c
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 17:59:03 -0300

    feat: create contentHome (cards, handleClickCard, select a candidate)

---
+++ src/components/ContentHome/index.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState, useContext } from "react";
+import * as S from "./styles";
+import { arrayCandidates } from "../../constant/arrayCandidates";
+import { AuthContext } from "../../context/context";
+
+interface ArrayCandidates {
+	id: number,
+	name: string,
+	idProfession: number,
+	image: string
+	selected: number
+}
+
+interface IPropsContentHome {
+	id: number;
+}
+
+export const ContentHome: React.FC<IPropsContentHome> = ({id}) => {
+	const [arrayListCandidate, setArrayListCandidate] = useState<ArrayCandidates[]>([{
+		id: 0,
+		name: "",
+		idProfession: 0,
+		image: "",
+		selected: 0
+	}]);
+	const { candidatesSelects, setCandidatesSelects } = useContext(AuthContext);
+	const [shuffle, setShuffle] = useState(false);
+	const [cardActive, setCardActive] = useState(1);
+	const [selectedItem, setSelectedItem] = useState(0);
+	useEffect(() => {
+		return setArrayListCandidate(
+			arrayCandidates.filter((item: ArrayCandidates) => {
+				if (item.idProfession === id && item.selected === 0) {
+					return true;
+				}
+				return false;
+			})
+		);
+	}, [arrayCandidates]);
+
+	const handleClickCard = (idCandidate: number) => {
+		setCandidatesSelects(
+			candidatesSelects.filter((item) => {
+				if (item.idProfession === id) {
+					item.idCandidate = idCandidate;
+					setSelectedItem(idCandidate);
+				}
+				return true;
+			})
+		);
+	};
+
+	return (
+		<>
+			<S.BtnShuffle
+				onClick={
+					() => {
+						setShuffle(!shuffle);
+						setTimeout(() => {
+
+							if (cardActive === 1) {
+								setCardActive(2);
+							} else {
+								setCardActive(1);
+							}
+						}, 200);
+						
+					}
+				}
+			>
+				<p>Embaralhar</p>
+			</S.BtnShuffle>
+			<S.ContentArea>
+				{arrayListCandidate.map((item, index) => {
+				
+					if (shuffle) {
+						if (index > 4) {
+							return (
+								<div
+									className={cardActive === 2 ? "card active 	selected" : "card disabled"}
+									key={item.id}
+									onClick={
+										() => {
+											handleClickCard(item.id);
+										}
+									}
+								>
+									<S.ImgWrapper
+										image={`url(${item.image})`}
+									/>
+									<p>{item.name}</p>
+									{
+										selectedItem === item.id
+											? <p className="select">SELECIONADO</p>
+											: ""
+									}
+								</div>
+							);
+						}
+					} else {
+						if (index < 5) {
+							return (
+								<div
+									className={cardActive === 1 ? "card active" : "card disabled"}
+									key={item.id}
+									onClick={
+										() => {
+											handleClickCard(item.id);
+										}
+									}
+								>
+									<S.ImgWrapper
+										image={`url(${item.image})`}
+									/>
+									<p>{item.name}</p>
+									{
+										selectedItem === item.id
+											? <p className="select">SELECIONADO</p>
+											: ""
+									}
+								</div>
+							);
+						}
+					}
+				})}
+			</S.ContentArea>
+		</>
+	);
+};
+
---
+++ src/components/ContentHome/styles.tsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+export const  ContentArea = styled.div`
+    min-height: 100vh;
+    min-width: 100vw;
+    height: 100%;
+    background-color: none; 
+    padding: 2rem;
+    display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+    .card{
+        width: auto;
+        height: 22rem;
+        padding: 2rem;
+        background-color: #121212; 
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        cursor: pointer;
+        border-radius: 1rem;   
+        margin-bottom: 2rem;
+        transition: all 1s ease-in-out;
+        p{
+            font-size: 1.5rem;
+            margin-top: 1rem;
+            color: #FFF;
+            text-align: center;
+            vertical-align: center;
+            flex: 1;
+        }
+    }
+
+    .select{
+        position: absolute;
+        width: auto;
+        text-align: center;
+        background-color: #12121277;
+        padding: 1rem;
+    }
+
+    .active{
+        visibility: visible;
+        opacity: 1;
+        transition: all .5s ease-in-out;
+    }
+    .disabled{
+        visibility: hidden;
+        transition: all 1s ease-in-out;
+        opacity: 0;
+    }
+
+    @media (max-width: 800px) {
+        grid-template-columns: 1fr
+    }
+`;
+
+interface IProprsImgwrapper {
+    image: string;
+}
+
+export const ImgWrapper = styled.div<IProprsImgwrapper>`
+    width: 100%;
+    height: 20rem;
+    border-radius: 1rem;   
+    background-image: ${props => props.image};
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+`;
+
+export const BtnShuffle = styled.div`
+    position: fixed;
+    bottom: 1rem;
+    right: 1rem;
+    background-color: #096688;
+    padding: .5rem 1rem ;
+    border-radius: 1rem;
+    cursor: pointer;
+    transition: all .5s ease-in-out;
+    p{
+        font-size: 1.8rem;
+        color: #FFF;
+    }
+    &&:hover {
+        background-color: #063a4d;
+    }
+`;
