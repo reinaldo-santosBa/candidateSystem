291a456e029de2a8213100bfa419950752ba0043
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 13:21:06 -0300

    feat: create for a element menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,13 +1,131 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import * as S from "./styles";
+import { arrayMenu } from "../../constant/arrayMenu";
+import { AuthContext } from "../../context/context";
+import {
+	AiOutlineClose
+
+} from "react-icons/ai";
+
+import {
+	FiMenu
+} from "react-icons/fi";
 interface IChildren {
-    children: React.ReactNode
+	children: React.ReactNode;
+	idMenu: number;
 }
-export const Menu: React.FC<IChildren> = ({ children}) => {
+
+interface ISubMenu {
+	id: number;
+	name: string;
+	
+}
+
+interface Iarray {
+	id: number;
+	name: string;
+	subName: string;	
+	subMenu: ISubMenu[];
+}
+
+export const Menu: React.FC<IChildren> = ({ children,idMenu }) => {
+	const [active, setActive] = useState(idMenu);
+	const [activeSubMenu, setActiveSubMenu] = useState(0);
+	const [positionLeftMenu, setPositionLeftMenu] = useState("0");
+	const [positionLeftBtn, setPositionLeftBtn] = useState("19.75rem");
+	const [iconOpenClose, setIconOpenClose] = useState(true);
+	const {
+		setIdProfession,
+	} = useContext(AuthContext);
 	return (
-		<S.AreaMenu>
-			{children}
-		</S.AreaMenu>
+		<>
+			<S.BtnOpenClose
+				
+				positionLeftBtn={positionLeftBtn}
+				onClick={
+					() => {
+						if (positionLeftMenu === "0") {
+							setPositionLeftMenu("-18.75rem");
+							setPositionLeftBtn("2rem");
+							setIconOpenClose(false);
+						} else {
+							setPositionLeftMenu("0");
+							setPositionLeftBtn("20.75rem");
+							setIconOpenClose(true);
+						}
+					}
+				}
+			>
+				{
+					iconOpenClose
+						? <AiOutlineClose />
+						: <FiMenu/>
+				}
+			</S.BtnOpenClose>
+			<S.AreaMenu
+				positionLeftMenu={positionLeftMenu}
+			>
+				{children}
+
+				<S.AreaItem>
+					{
+						arrayMenu.map((item: Iarray) => {
+							return (
+								<>
+									<S.AreaItemBtn
+										key={item.id}
+									>
+										<div className={active === item.id ? "decorationSelected" : ""} />
+
+										<a
+											className={active === item.id ? "activeBtn" : ""}
+											onClick={
+												() => {
+													setActive(item.id);
+													if (item.subMenu.length === 0) {
+														setActiveSubMenu(0);
+													}
+												}
+											}
+										>
+											{item.name}
+											<p>
+												{item.subName}
+											</p>
+										</a>
+									</S.AreaItemBtn>
+									{
+										active === item.id && item.subMenu.length > 0
+											?
+											item.subMenu.map((subItem: ISubMenu) => {
+												return (
+													<S.AreaItemSubBtn
+													
+														key={subItem.id}
+														onClick={
+															() => {
+																setActiveSubMenu(subItem.id);
+																setIdProfession(subItem.id);
+															}
+														}
+													>
+														<a
+															className={activeSubMenu === subItem.id ? "activeBtn" : ""}
+														>
+															{subItem.name}
+														</a>
+													</S.AreaItemSubBtn>
+												);
+											})
+											: ""
+									}
+								</>
+							);
+						})
+					}
+				</S.AreaItem>
+			</S.AreaMenu>
+		</>
 	);
 };
 
--- src/components/menu/styles.tsx
+++ src/components/menu/styles.tsx
@@ -1,5 +1,139 @@
 import styled from "styled-components";
 
-export const  AreaMenu = styled.div`
+interface IMenuStyles {
+    positionLeftMenu: string;
+}
 
+interface IbtnStyle {
+    positionLeftBtn: string;
+
+}
+
+export const AreaMenu = styled.nav<IMenuStyles>`
+    min-height: 100vh;
+    max-height: 100vh;
+    overflow-y: auto;
+    overflow-x: hidden;
+    width: 18.75rem;
+    background-color: #0A0A0A;
+    box-shadow: 1px 1px 1px #000;
+    position: relative;
+    top: 0;
+    left: ${props => props.positionLeftMenu ? props.positionLeftMenu : 0 };
+    transition: all 0.5s ease-in-out;
+`;
+
+export const BtnOpenClose = styled.div <IbtnStyle>`
+    color: #096688;
+    font-size: 3rem;
+    position: absolute;
+    top: 1.5rem;
+    left: ${props => props.positionLeftBtn};
+    z-index: 100;
+    cursor: pointer;
+    transition: all 0.5s ease-in-out;
+`;
+
+
+export const AreaItem = styled.ul`
+    width: 100%;
+    height: auto;
+    list-style: none;
+    padding: 0;
+`;
+
+
+export const AreaItems = styled.li`
+    margin: .8rem 0;
+    width: 100%;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    position: relative;
+    justify-content: center;
+    transition: all 0.5s ease-in-out;
+    &&:hover{ 
+        a{
+            color: #096688;
+        }
+        div{
+            background-color: #096688;
+            width: 1rem;
+            position: absolute;
+            border-radius: 0% .5rem .5rem 0;  
+            height: 2rem;
+            left: 0;
+        }
+    }
+
+`;
+
+export const  AreaItemBtn = styled.li`
+    margin: 1.5rem 0;
+    width: 100%;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    position: relative;
+    justify-content: center;
+    transition: all 0.5s ease-in-out;
+    &&:hover{ 
+        a{
+            color: #096688;
+        }
+        div{
+            background-color: #096688;  
+        }
+    }
+    .decorationSelected {
+        background-color: #096688;
+    }
+    div{ 
+        transition: all 0.5s ease-in-out;
+        background-color: none;
+        width: 1rem;
+        position: absolute;
+        border-radius: 0% .5rem .5rem 0;  
+        height: 110%;
+        left: 0;
+        transition: all 0.5s ease-in-out;
+    }
+    a{
+            transition: all 0.5s ease-in-out;
+
+        color: #FFF;
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 1.5rem;
+        letter-spacing: .4rem;
+        text-align: center;
+    }
+
+
+    .activeBtn{
+        color: #096688;
+    }
+`;
+
+export const AreaItemSubBtn = styled.li`
+    margin: .5rem 0;
+    width: 100%;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    position: relative;
+    justify-content: center;
+    transition: all 0.5s ease-in-out;
+    &&:hover{ 
+        a{
+            color: #096688;
+            font-size: 1.5rem;
+
+        }
+    }
+    color: #FFF;
+    .activeBtn{
+        color: #096688;
+        font-size: 1.5rem;
+    }
 `;
