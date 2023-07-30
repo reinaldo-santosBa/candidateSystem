2e0c4d2ede5e1d18e3f7e8f57ea753a814038664
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 13:20:18 -0300

    feat: create array  of items menu

---
+++ src/constant/arrayMenu.ts
@@ -0,0 +1,47 @@
+export const arrayMenu = [
+	{
+		id: 1,
+		name: "Home",
+		subName: "",
+		subMenu:[]
+	},
+	{
+		id: 2,
+		name: "Candidato",
+		subName: "Escolhido",
+		subMenu: []
+	},
+	{
+		id: 3,
+		name: "Todos",
+		subName: "Candidatos",
+		subMenu: []
+	},
+	{
+		id: 4,
+		name: "Processo",
+		subName: "Seletivo",
+		subMenu: [
+			{
+				id: 1,
+				name: "Front - End"
+			},
+			{
+				id: 2,
+				name: "Back - End"
+			},
+			{
+				id: 3,
+				name: "Full Stack"
+			},
+			{
+				id: 4,
+				name: "Designer Figma"
+			},
+			{
+				id: 5,
+				name: "Banco de Talentos"
+			},
+		]
+	},
+];
