bac33995e167181edeb90c63a6a8d5bd2b991cd1
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-07-30 22:05:22 -0300

    feat: call idprofession

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { Container } from "../../components/container";
 import { Menu } from "../../components/menu";
 import { Logo } from "../../components/logo";
-import { ContentHome } from "../../components/ContentHome";
+import { ContentHome } from "../../components/contentHome";
 
 export const Home: React.FC = () => {
 	const { id } = useParams();
@@ -11,6 +11,7 @@ export const Home: React.FC = () => {
 		<Container>
 			<Menu
 				idMenu={1}
+				idProfession={Number(id)}
 			>
 				<Logo/>
 			</Menu>
